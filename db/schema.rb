8e59859410b3967ac25a2f62b24f329cfaa7acc5
khangkuro <khangkuro96@gmail.com>
2019-12-16 15:27:11 +0700

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_163842) do
+ActiveRecord::Schema.define(version: 2019_12_16_081804) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "create_at"
     t.datetime "update_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end

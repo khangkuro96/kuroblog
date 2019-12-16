447c6a22d4a9ecc337df220abdfb3e92b3382939
khangkuro <khangkuro96@gmail.com>
2019-12-16 09:24:28 +0700

    Create a page show the input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,20 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def create 
-       #render plain: params[:articles].inspect
        @article = Article.new(article_params)
-       @article.save
-       redirect_to articles_show(@article)
+       if @article.save
+         flash[:notice] = "Article was successfully created"
+         redirect_to article_path(@article)
+       else
+            render 'new'
+       end
+    end
+    def show
+        @article = Article.find(params[:id])
     end
 
     private
     def article_params
-        params.require(:articles).permit(:title,:description)
+        params.require(:article).permit(:title, :description)
     end
 end
